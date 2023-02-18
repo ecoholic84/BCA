7f16394f2b0e9bab9e035dcfe3be835a1edb4a5b
ecoholic <akashanil084@gmail.com>
2023-02-18 17:18:29 +0530

    > Replaced pgrm5 > Added algorithm for pgrm10.

--- sem1/05_reverseOfNumberAndSum.c
+++ sem1/05_reverseOfNumberAndSum.c
@@ -1,37 +1,34 @@
 #include <stdio.h>
-// #include <conio.h>
+// include <conio.h>
 void main()
 {
-    int n,d,s=0,rev=0;
+    int a,b,d,s=0;
     // clrscr();
-    printf("Enter a number: ");
-    scanf("%d",&n);
-    do
+    printf(" Enter a number: ");
+    scanf("%d",&a);
+    printf("Reverse: ");
+
+    while(a>0)
     {
-        d=n%10;
-        s=s+d;
-        rev=rev*10+d;
-        n=n/10;
+        b=a%10;
+        s=s+b;
+        printf("%d",b);
+        a=a/10;
     }
-    while (n>0);
-    printf("Reverse=%d",rev);
-    printf("\nSum=%d",s);
+    printf("\nSum: %d",s);
   // getch();
-    
 }
 
 /*
 Algorithm:
-
 Step 1: Start
-Step 2: Declare variables n,d,s,rev
-Step 3: Read values for n
-Step 4: s=0, rev=0
-Step 5: If n>0, go to step 6, else go to step 10
-Step 6: Calculate d = n%10
-Step 7: Calculate s = s+d
-Step 8: Calculate rev = rev*10+d
-Step 9: Calculate n = n/10, go to step 5
-Step 10: Display rev and 5
-Step 11: Stop
+Step 2: Declare variables a,b,d,s=0
+Step 3: Scan a
+Step 4: Repeat steps 5 to 8 until a>0
+Step 5: b=a%10
+Step 6: s=s+b
+Step 7: Print b
+Step 8: a=a/10
+Step 9: Print s
+Step 10: Stop
 */
--- sem1/10_linearSearchOnArray.c
+++ sem1/10_linearSearchOnArray.c
@@ -30,3 +30,24 @@ void main()
     }
   // getch();
 }
+
+/*
+Algorithm:
+
+Step 1: Start
+Step 2: Declare a[10],n,i,e
+Step 3: Scan n
+Step 4: i=0
+step 5: If i<n. goto step 6, else go to step 8
+Step 6: Scan a[i]
+Step 7: i++, goto step 5
+Step 8: Read e
+Step 9: i=0
+Step 10: If i<n, goto step 11, else goto step 14
+Step 11: If a[i]==e, goto step 12, else goto step 13
+Step 12: Display e is fount in position i, break, goto step 14
+Step 13: i++, goto step 10
+Step 14: If i==h, goto step 15 else goto step 16
+Step 15: Display e is not found
+Step 16: Stop
+*/
