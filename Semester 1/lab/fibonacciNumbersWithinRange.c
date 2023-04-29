8fd551968f64d2ec222a73404950bcac64b31b60
ecoholic <akashanil084@gmail.com>
2023-04-29 10:46:47 +0530

    Program 1 added to lab questions

--- Semester 1/lab/fibonacciNumbersWithinRange.c
+++ Semester 1/lab/fibonacciNumbersWithinRange.c
@@ -5,25 +5,24 @@ The Fibonnaci series is given as 1 1 2 3 5 8 13 21 34
 */
 
 #include <stdio.h>
+
 int main()
 {
-    int i, a=0, b=1, c=0, count=0;
-    for(i=0; i<400; i++)
+    int a = 1, b = 1, c, count = 0;
+    printf("Fibonacci numbers in the range 1 to 400:\n");
+    printf("%d ", a);
+    while (b <= 400)
     {
-        printf("%d%d\t",a,b);
-        c=a+b;
-        while(c <= 400)
+        printf("%d ", b);
+        if (b % 3 == 0)
         {
-            if(c % 3 == 0)
-            {
-                count=count+1;
-            }
-            printf("%d\t",c);
-            a=b;
-            b=c;
-            c=a+b;
+            count++;
         }
+        c = a + b;
+        a = b;
+        b = c;
     }
-    printf("Total multiples of 3: %d", count);
+    printf("\nNumber of Fibonacci numbers that are multiples of 3: %d", count);
     return 0;
 }
+
