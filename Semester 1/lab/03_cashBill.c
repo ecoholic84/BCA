397626a5fe1a96f8f73706b4a7ba0505e20e3278
ecoholic <akashanil084@gmail.com>
2023-05-10 17:01:46 +0530

    Added its question too

--- Semester 1/lab/03_cashBill.c
+++ Semester 1/lab/03_cashBill.c
@@ -1,3 +1,19 @@
+/*
+A company produces three types of products, Fan, AC, and TV.
+The company gives discount for bulk purchases.
+The discount rate is as follows:
+
+No of items ordered     Product     Discount rate
+20 or more              Fan          12%
+10 or more              AC           15%
+5 or more               TV           08%
+
+Otherwise there is no discount.
+Assuming the order can be for one item or more than one item at a time,
+Write a C program to prepare a Cash bill for a customer who buys these products.
+Use appropriate decision making statement in your program.
+*/
+
 #include <stdio.h>
 void main()
 {
